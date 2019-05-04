feb0477be030f37b689ac75892299a20871a4b34
Jansen <13516011@std.stei.itb.ac.id>
2019-05-05 01:10:19 +0700

    Updates

---
+++ precache-manifest.5273986fede965e87cbdc087166b4218.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abdc9be5bb3672cc18062e57bdc3577",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "134bfdeb4fae81933445",
+    "url": "/resume/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "93a8b01f55332594dbf8",
+    "url": "/resume/static/css/main.1b0a56bf.chunk.css"
+  },
+  {
+    "revision": "134bfdeb4fae81933445",
+    "url": "/resume/static/js/2.ba39a63c.chunk.js"
+  },
+  {
+    "revision": "93a8b01f55332594dbf8",
+    "url": "/resume/static/js/main.2bddd051.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "3815a9caf31546211741bfe21fa828d5",
+    "url": "/resume/static/media/face-recognition.3815a9ca.jpg"
+  },
+  {
+    "revision": "7e9b4597c36578945fc637a3c06bd498",
+    "url": "/resume/static/media/me.7e9b4597.jpg"
+  },
+  {
+    "revision": "ee57e66e5709b4c32064f623368a97c4",
+    "url": "/resume/static/media/visual-studio.ee57e66e.png"
+  }
+]);
